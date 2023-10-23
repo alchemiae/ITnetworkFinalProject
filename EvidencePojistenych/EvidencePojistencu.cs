c87be650f332835c2ab1ce29d3c4623505b2b9aa
Lukáš Praus <lukas.praus23@gmail.com>
2023-10-23 18:34:54 +0200

    Přidejte soubory projektu.

---
+++ EvidencePojistenych/EvidencePojistencu.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencePojistenych
+{
+    class EvidencePojistencu
+    {
+        Dictionary<string, Pojistenec> evidence = new Dictionary<string, Pojistenec>();
+
+        public void VypisVsechnyPojistence()
+        {
+            Console.WriteLine("Seznam pojištěnců:");
+            foreach (Pojistenec pojistenec in evidence.Values)
+            {
+                Console.WriteLine(pojistenec);
+            }
+            Console.WriteLine("\nPokračujte libovolnou klávesou...");
+            Console.ReadKey();
+        }
+
+        public void NajdiPojistence()
+        {
+            Console.WriteLine("Zadejte křestní jméno hledaného pojištěnce:");
+                string hledaneJmeno = Console.ReadLine().ToLower().Trim();
+            Console.WriteLine("Zadejte příjmení hledaného pojištěnce:");
+                string hledanePrijmeni = Console.ReadLine().ToLower().Trim();
+            string klic = hledaneJmeno + " " + hledanePrijmeni.ToLower();
+
+            if (evidence.ContainsKey(klic))
+            {
+                Console.WriteLine(evidence[klic]);
+            }
+            else
+            {
+                Console.WriteLine("Pojištěnec nebyl nalezen.");
+            }
+
+            Console.WriteLine("\nPokračujte libovolnou klávesou...");
+            Console.ReadKey();
+        }
+
+        public void PridejPojistence()
+        {
+            Console.WriteLine("Zadejte jméno pojištěného:");
+                string jmeno = Console.ReadLine().Trim();
+            Console.WriteLine("Zadejte příjmení pojištěného:");
+                string prijmeni = Console.ReadLine().Trim();
+            Console.WriteLine("Zadejte telefonní číslo:");
+                int telefon;
+                while (!int.TryParse(Console.ReadLine(), out telefon))
+                {
+                    Console.WriteLine("Neplatné telefonní číslo.");
+                }
+            Console.WriteLine("Zadejte věk:");
+                int vek;
+                while (!int.TryParse(Console.ReadLine(), out vek))
+                {
+                    Console.WriteLine("Neplatný věk.");
+                }
+
+            string klic = jmeno.ToLower() + " " + prijmeni.ToLower();
+            evidence[klic] = new Pojistenec(jmeno, prijmeni, telefon, vek);
+
+            Console.WriteLine("\nData byla uložena. Pokračujte libovolnou klávesou...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EvidencePojistenych/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EvidencePojistenych
+{
+    class Program
+    {
+        static void VypisMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Evidence pojištěných");
+            Console.WriteLine("---------------------------\n");
+
+            Console.WriteLine("Vyberte si akci:");
+            Console.WriteLine("1 - Přidat nového pojištěného");
+            Console.WriteLine("2 - Vypsat všechny pojištěné");
+            Console.WriteLine("3 - Vyhledat pojištěného");
+            Console.WriteLine("4 - Konec");
+        }
+
+        static void Main(string[] args)
+        {
+            EvidencePojistencu evidence = new EvidencePojistencu();
+
+            sbyte volba = 0;
+
+            while (volba != 4)
+            {
+                VypisMenu();
+
+                while (!sbyte.TryParse(Console.ReadLine(), out volba))
+                    Console.WriteLine("Neplatná volba");
+                
+                switch (volba)
+                {
+                    case 1:
+                        evidence.PridejPojistence();
+                        break;
+                    case 2:
+                        evidence.VypisVsechnyPojistence();
+                        break;
+                    case 3:
+                        evidence.NajdiPojistence();
+                        break;
+                    case 4:
+                        Console.WriteLine("Nashledanou!");
+                        return;
+                }    
+            }
+        }
+    }
+}
